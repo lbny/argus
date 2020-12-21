7be24b95d7cea01f6f1b25c4b2566b8d1cfa5f22
lbny <lucas.bony@hec.edu>
2020-12-21 11:47:56 +0100

    smdl

--- 2_run_experiments.py
+++ 2_run_experiments.py
@@ -19,6 +19,11 @@ Two feature pipelines are run separately:
 Basically, features which are parametrized by variables
 which you intend to search by hyperoptimization should be incroporated
 in the experiment features.
+
+Data structure for the datasets follows : 
+- dictionary of pd.DataFrame (meaning each dataset must be named)
+- prior to shared feature pipeline, turn into dictionary of tuples :
+    pd.DataFrame, np.array, scipy.sparse.csr_matrix
 """
 #############################################
 #############################################
@@ -31,7 +36,24 @@ source_filepaths:
     valid_dataset_filepath: ./datasets/jane_street/preprocessed/valid.csv
     test_dataset_filepath: ./datasets/jane_street/preprocessed/test.csv
     features_filepath: ./datasets/jane_street/features.csv
-seed: 
+
+seed: 1234
+
+shared_features:
+    tf_idf:
+        text_colname: text
+        sublinear_tf: true
+        min_df: 10
+
+experiment_features:
+    tf_idf:
+        text_colname: 
+            type: fixed
+            val: text
+        min_df:
+            type: randint
+            val: [5, 25]
+        
 
 
 '''
@@ -40,18 +62,20 @@ NB_WORKERS = 1
 
 # Initialization
 pandarallel.initialize(nb_workers=NB_WORKERS)
-data_config = yaml.load(PARAMETERS, Loader=Loader)
+config = yaml.load(PARAMETERS, Loader=Loader)
 
 #############################################
 #############################################
 # 2. Parameters Consistency
 
+# %%
 #############################################
 #############################################
 # 3. Loading datasets
+df_dict = {}
 
 for dataset_name, dataset_filepath in config['source_filpaths'].items():
-
+    df_dict[dataset_name] = pd.read_csv(dataset_filepath)
 
 #############################################
 #############################################
@@ -64,6 +88,15 @@ def split_datasets()
 # 4. Shared features pipeline
 
 # Initialize array and sparse features
+dataset_dict = {}
+
+for dataset_name in df_dict.keys():
+    dataset_tuple = (d
+        f_dict[dataset_name], 
+        np.empty(shape=(df_dict[dataset_name].shape[0])),
+        csr_matrix()
+    )
+    dataset_dict[dataset_name] 
 
 #############################################
 #############################################
